ddfe0aae366978afcb5ec1a21b20c3dbd72a073f
Your Name <traviskaine>
2024-07-25 17:06:04 -0500

    logo needs work

--- ranchsupply-frontend/src/components/home/HomeContent.jsx
+++ ranchsupply-frontend/src/components/home/HomeContent.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Button, Col, Container, Nav, Row } from "react-bootstrap";
+import "../../App.css"
+
 // import { useNavigate } from "react-router-dom";
 
 const HomeContent = () => {
   return (
-    <>
+    <div className="background-image">
       <Container className="pt-4 pb-4">
         <Row>
           <Col
@@ -14,19 +16,24 @@ const HomeContent = () => {
           >
             <h1>Ranch Supply</h1>
             <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Corrupti, facere beatae fuga cupiditate enim nobis dicta, fugit
-              obcaecati ea repudiandae asperiores fugiat, voluptate voluptatum
-              cum quas. Repellat voluptate earum accusantium. Lorem ipsum dolor
-              sit amet consectetur adipisicing elit. Ipsum expedita rerum, aut
-              magni fugit magnam fuga tempore eveniet ullam eum, nostrum eos
-              ducimus! Magni, quod repudiandae ratione maiores nobis sit.
-              <br />
-              <b />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
-              laboriosam, officiis velit recusandae omnis ratione reiciendis
-              incidunt dolorem quis? Perspiciatis possimus eius inventore dolor
-              vel itaque ad magni porro incidunt!
+            Welcome to Ranch Supply, your one-stop shop for all your ranching and outdoor 
+            needs. We specialize in providing top-quality farming equipment, from robust 
+            tractors to essential tools that make your daily tasks easier and more efficient. 
+            Our extensive inventory ensures that whether you are cultivating your fields or 
+            maintaining your property, you will find the right equipment to get the job done. 
+            At Ranch Supply, we understand the hard work and dedication it takes to run a 
+            successful farm, which is why we offer only the best brands and products trusted 
+            by professionals.</p>
+              <p>
+              In addition to our comprehensive selection of farming equipment, we also 
+              cater to your outdoor lifestyle. Our range of farm clothing is designed for 
+              comfort and durability, keeping you prepared for any weather conditions while 
+              working outside. But it's not all work and no play at Ranch Supply. We also 
+              offer a variety of outdoor recreational items to help you unwind and enjoy 
+              your time off. From hunting gear to camping essentials, we have everything 
+              you need to make the most of your outdoor adventures. Explore our store today 
+              and discover how Ranch Supply can enhance both your work and leisure time on 
+              the ranch.
             </p>
             {/* If user is logged in dont show login/register button otherwise show buttons  */}
             {false ? (
@@ -44,7 +51,7 @@ const HomeContent = () => {
           </Col>
           <Col xs={12} md={6}>
             <img
-              // src="src/assets/hero-image.png"
+              src="https://m.media-amazon.com/images/I/814gr-bR9-L._AC_SX679_.jpg"
               style={{ width: "100%" }}
               fluid="true"
               className="d-inline-block align-top"
@@ -53,7 +60,7 @@ const HomeContent = () => {
           </Col>
         </Row>
       </Container>
-    </>
+      </div>
   );
 };
 
